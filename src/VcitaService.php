3c39cecd4f6833f813c98ab6d3c5cb21e2ac6172
Chirag Goti <gotichirag2011@gmail.com>
2023-02-27 19:18:41 +0530

    feat: add client api in service file

--- src/VcitaService.php
+++ src/VcitaService.php
@@ -7,14 +7,70 @@ class VcitaService {
     
     private $base_url;
     private $token;
+    private $http;
 
     public function __construct($base_url, $token)
     {
         $this->base_url = $base_url;
         $this->token = $token;
+        $this->http = $this->http();
     }
 
     function http(){
         return Http::withToken($this->token)->baseUrl($this->base_url);
+    }
+
+    public function getForms($access_by = null) {
+        $response = $this->http->get('/platform/v1/forms');
+
+        if($response->successful()) {
+            $data = $response->json($access_by);
+            return collect($data);
+        }
+    }
+
+    public function allClients($access_by = null) {
+        $response = $this->http->get('/platform/v1/clients');
+
+        if($response->successful()) {
+            $data = $response->json($access_by);
+            return collect($data);
+        }
+    }
+
+    public function createClients($data,$access_by = null) {
+        $response = $this->http->post('/platform/v1/clients');
+
+        if($response->successful()) {
+            $data = $response->json($access_by);
+            return collect($data);
+        }
+    }
+
+    public static function getClient($id, $access_by = null){
+        $response = $this->http()->get('/platform/v1/clients/'.$id);
+
+        if($response->successful()) {
+            $data = $response->json($access_by);
+            return collect($data);
+        }
+    }
+
+    public static function updateClient($id, $data, $access_by = null){
+        $response = $this->http()->put('/platform/v1/clients/'.$id, $data);
+
+        if($response->successful()) {
+            $data = $response->json($access_by);
+            return collect($data);
+        }
+    }
+
+    public static function deleteClient($id, $access_by = null){
+        $response = $this->http()->delete('/platform/v1/clients/'.$id);
+
+        if($response->successful()) {
+            $data = $response->json($access_by);
+            return collect($data);
+        }
     }
 }
